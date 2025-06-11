86eeaec23675e25cbd503d5a802b7e515923ce42
ntphi360 <ntphi360@gmail.com>
2025-06-11 17:55:31 +0700

    Add inertia

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+// import path from 'path';
+
 
 export default defineConfig({
     plugins: [
@@ -9,5 +12,11 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
+        vue({
+            template:{
+                base: null,
+                includeAbsolute: false,
+            }
+        }),
     ],
 });
