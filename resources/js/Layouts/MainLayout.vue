cf88f4d91d6a383649b4794a1683996f9935b9b5
ntphi360 <ntphi360@gmail.com>
2025-06-18 18:25:51 +0700

    flashSuccess

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,13 +1,22 @@
 <template>  
-
-  
-  
+   <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+   </div>
   <slot>Default</slot>
 </template>
 
 <script setup>
-  import { Link } from '@inertiajs/vue3'
-  import { ref } from 'vue';
-
-
+  import { Link, usePage } from '@inertiajs/vue3'
+  import { computed, ref } from 'vue';
+  
+  const page = usePage();
+  const flashSuccess = computed(() => page.props.flash.success);
 </script>
+
+<style scoped>
+  .success{
+    background-color: green;
+    color:white
+  }
+</style>
+
