c4ae07c766b67244519bdc76e649e4d5e7ccfeae
ntphi360 <ntphi360@gmail.com>
2025-06-24 18:59:54 +0700

    Listing detail

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -2,7 +2,8 @@
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
     <Box v-for="listing in listings" :key="listing.id">
       <Link :href="route('listing.show',{listing: listing.id})">
-        <ListingAddress :listing="listing"/>
+        <ListingSpace :listing="listing"/>
+        <ListingAddress :listing="listing" class="text-gray-500 "/>
       </Link>
       <div>
         <Link :href="route('listing.edit',{listing: listing.id})">Edit</Link> &nbsp;
@@ -16,6 +17,7 @@
   import { Link } from '@inertiajs/vue3'
   import ListingAddress from '@/Components/ListingAddress.vue';
   import Box from '@/Components/UI/Box.vue';
+  import ListingSpace from '@/Components/ListingSpace.vue';
   const props = defineProps({
     listings: Array,
   })
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,9 +1,8 @@
 <template>
-  <p>{{ listing.id }} {{ listing.street }} {{ listing.street_nr }} {{ listing.city }}, for {{ listing.price }}</p>
+  <p>{{ listing.id }}. {{ listing.street }} {{ listing.street_nr }} {{ listing.city }}, for {{ listing.price }}</p>
 </template>
 
 <script setup>
-// import { Link } from '@inertiajs/vue3'
   const props = defineProps({
     listing: Object,
   })
