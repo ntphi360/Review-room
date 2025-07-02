ff7ffcd77a7bd99adc0199e9e3d7e30cac6e22a3
ntphi360 <ntphi360@gmail.com>
2025-07-02 19:17:23 +0700

    Styling show

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,8 +1,36 @@
 <template>
-  <p>{{ listing.id }}. {{ listing.street }} {{ listing.street_nr }} {{ listing.city }}, for {{ listing.price }}</p>
+  <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+    <Box class="md:col-span-7 flex justify-center items-center">
+      <div class="w-full text-center font-medium text-gray-500">No images</div>
+    </Box>
+    <div class="md:col-span-5 flex flex-col gap-4"> 
+      <Box>
+        <template #header>
+          Basic info
+        </template>
+        <Price :price="listing.price" class="text-xl font-bold"/>
+        <ListingSpace :listing="listing"/>
+        <ListingAddress :listing="listing" class="text-gray-500 "/>
+      </Box>
+      <Box>
+         <template #header>
+          Offer
+        </template>
+        Make an offer
+      </Box>
+    </div>
+  </div>
 </template>
 
+
+
+
+
 <script setup>
+  import ListingAddress from '@/Components/ListingAddress.vue';
+  import Box from '@/Components/UI/Box.vue';
+  import ListingSpace from '@/Components/ListingSpace.vue';
+  import Price from '@/Components/Price.vue';
   const props = defineProps({
     listing: Object,
   })
