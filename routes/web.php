44f2a0208269d239300943da6b21962488fea9ee
ntphi360 <ntphi360@gmail.com>
2025-07-07 18:19:47 +0700

    AuthController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListingController;
 
 Route::resource('listing',ListingController::class)
     ->only(['index','show','create','store','edit','update','destroy']);
+    
+Route::get('login' ,[AuthController::class,'create'])
+    ->name('login');
+Route::post('login' ,[AuthController::class,'store'])
+    ->name('login.store');
+Route::get('logout' ,[AuthController::class,'destroy'])
+    ->name('logout');
