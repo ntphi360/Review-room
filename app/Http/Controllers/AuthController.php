4221b3b888110b373a087b89386f0d3b1073e6ae
ntphi360 <ntphi360@gmail.com>
2025-07-07 18:35:45 +0700

    Login page and authRequest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AuthRequest;
 use Illuminate\Http\Request;
+
 
 class AuthController extends Controller
 {
     public function create() { 
-        
+        return inertia('Auth/Login');
     }
     
-    public function store(){
-        
+    public function store(AuthRequest $request){
+        dd($request->all());
     }
 
     public function destroy(){
